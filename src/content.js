4f241e6c7901b26bc74bdbb9ab4608c03629503b
Anubhav Chawla <anubhavchawla02@gmail.com>
2023-05-28 21:14:07 +0530

    Initial commit

---
+++ src/content.js
@@ -0,0 +1,14 @@
+const Content = {
+    name: "Anubhav Chawla",
+    email: "anubhavchawla02@gmail.com",
+    profile_icon: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png",
+    github: "https://github.com/anubhavchawla2071",
+    linkedin: "https://www.linkedin.com/in/anubhavchawla02/",
+    bio: "To be or not to be",
+    profile: [],
+    education: [],
+    professional_experience: [],
+    skills: []
+};
+
+export default Content;
